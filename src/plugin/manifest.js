f3ecd655d9f1f148ef7fa20a23c2bacb56cd7649
cyao <cyao.sh@gmail.com>
2025-02-17 17:57:39 +0800

    change color for dev badge

--- src/plugin/manifest.js
+++ src/plugin/manifest.js
@@ -66,8 +66,8 @@ async function addDevBadge(iconPath) {
     <svg width="${size}" height="${size}" xmlns="http://www.w3.org/2000/svg">
       <defs>
         <linearGradient id="badge" x1="0%" y1="0%" x2="100%" y2="100%">
-          <stop offset="0%" stop-color="#4A90E2" />
-          <stop offset="100%" stop-color="#357ABD" />
+          <stop offset="0%" stop-color="#FF6B6B" />
+          <stop offset="100%" stop-color="#DC3545" />
         </linearGradient>
       </defs>
       
